23133671cbf54e6564a0b924759c702b2724bffb
kostyanok <ekaterina.kostyanok@gmail.com>
2018-08-13 15:27:31 +0300

    Создан класс Point и метод для вычисляет расстояние между двумя точками

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,8 +1,16 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-
 	public static void main (String[] args){
-		System.out.println ("Hello, world!");
-    }	
+	Point p1  = new Point (-1,3);
+	Point p2  = new Point (6,2);
+	Point d = new Point (-1,3,6,2);
+
+    System.out.println(distance(p1,p2));
+    System.out.println(d.distance());
+}
+	public static double distance(Point p1, Point p2) {
+		return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
+
+	}
 }

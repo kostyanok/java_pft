5eb4b3ac45ddf938898628b1ac2a1d7594acc01e
kostyanok <ekaterina.kostyanok@gmail.com>
2018-08-14 16:20:13 +0300

    Создан тест testNewContactCreation и сделан рефакторинг класса NewContactCreationTests

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/ContactData.java
@@ -0,0 +1,49 @@
+package ru.stqa.pft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String lastname;
+  private final String address;
+  private final String homePhoneNumber;
+  private final String mobilePhoneNumber;
+  private final String email;
+  private final String email2;
+
+  public ContactData(String firstname, String lastname, String address, String homePhoneNumber, String mobilePhoneNumber, String email, String email2) {
+    this.firstname = firstname;
+    this.lastname = lastname;
+    this.address = address;
+    this.homePhoneNumber = homePhoneNumber;
+    this.mobilePhoneNumber = mobilePhoneNumber;
+    this.email = email;
+    this.email2 = email2;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public String getHomePhoneNumber() {
+    return homePhoneNumber;
+  }
+
+  public String getMobilePhoneNumber() {
+    return mobilePhoneNumber;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getEmail2() {
+    return email2;
+  }
+}

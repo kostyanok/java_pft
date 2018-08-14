bc4dcd47d0506ffbdd89b5a552611e50b63282dd
kostyanok <ekaterina.kostyanok@gmail.com>
2018-08-14 10:17:49 +0300

    Переделан метод distance класса Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,13 +1,14 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
+
 	public static void main (String[] args){
+
 	Point p1  = new Point (-1,3);
 	Point p2  = new Point (6,2);
-	Point d = new Point (-1,3,6,2);
 
     System.out.println(distance(p1,p2));
-    System.out.println(d.distance());
+    System.out.println(p1.distance(p2));
 }
 	public static double distance(Point p1, Point p2) {
 		return Math.sqrt((p2.x - p1.x)*(p2.x - p1.x) + (p2.y - p1.y)*(p2.y - p1.y));
--- sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/Point.java
@@ -3,23 +3,15 @@ package ru.stqa.pft.sandbox;
 public class Point {
   public double x;
   public double y;
-  public double x1;
-  public double y1;
 
 
   public Point (double x, double y) {
     this.x = x;
     this.y = y;
   }
-  public Point (double x, double y, double x1, double y1) {
-    this.x = x;
-    this.y = y;
-    this.x1 = x1;
-    this.y1 = y1;
-  }
 
-  public double distance() {
-    return Math.sqrt((this.x1 - this.x)*(this.x1 - this.x) + (this.y1 - this.y)*(this.y1 - this.y));
+  public double distance( Point p2) {
+    return Math.sqrt((this.x - p2.x)*(this.x - p2.x) + (this.y - p2.y)*(this.y - p2.y));
 
   }
 

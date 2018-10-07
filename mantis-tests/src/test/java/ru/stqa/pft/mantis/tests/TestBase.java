75743f3e06d9d30d80fe3495556e8ddb46169450
kostyanok <34420907+kostyanok@users.noreply.github.com>
2018-10-07 11:29:37 +0300

    captcha doesn't disappear

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/TestBase.java
@@ -3,7 +3,10 @@ package ru.stqa.pft.mantis.tests;
 import org.openqa.selenium.remote.BrowserType;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeSuite;
-import ru.stqa.ptf.mantis.appmanager.ApplicationManager;
+import ru.stqa.pft.mantis.appmanager.ApplicationManager;
+
+import java.io.File;
+import java.io.IOException;
 
 public class TestBase {
   protected static ApplicationManager app = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
@@ -11,10 +14,12 @@ public class TestBase {
   @BeforeSuite
   public void setUp() throws Exception {
     app.init();
+    app.ftp().upload(new File("src/test/resources/config_inc.php"), "config_inc.php", "config_inc.php.bak");
   }
 
-  @AfterSuite
-  public void tearDown() {
+  @AfterSuite (alwaysRun = true)
+  public void tearDown() throws IOException {
+    app.ftp().restore("config_inc.php.bak", "config_inc.php" );
     app.stop();
   }
 

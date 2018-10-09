e54e42808928252e282ccd57ef701d7c7af88ab7
kostyanok <34420907+kostyanok@users.noreply.github.com>
2018-10-09 23:19:50 +0300

    testResetPassword is changed

--- mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ResetPasswordTest.java
+++ mantis-tests/src/test/java/ru/stqa/pft/mantis/tests/ResetPasswordTest.java
@@ -9,33 +9,37 @@ import ru.stqa.pft.mantis.model.UserData;
 import ru.stqa.pft.mantis.model.Users;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 
 import static org.testng.Assert.assertTrue;
 
 public class ResetPasswordTest extends TestBase {
 
     @BeforeMethod
-    public void startMailServer(){
+    public void startMailServer() {
         app.mail().start();
     }
 
     @Test
-    public void testLogin() throws IOException {
+    public void testResetPassword() throws IOException {
         app.sessionHelper().login("administrator", "root");
         app.goTo().ManageUserPage();
         Users before = app.db().users();
-        UserData selectedContact = before.iterator().next();
-        app.user().selectById(selectedContact.getId());
+        Set<UserData> usersWithoutAdmin = app.db().users().withoutUser(before, 1);
+        UserData selectedUser = usersWithoutAdmin.iterator().next();
+        app.user().selectById(selectedUser.getId());
         app.user().resetPassword();
         List<MailMessage> mailMessages = app.mail().waitForMail(1, 1000);
-        String confirmationLink = app.user().findConfirmationLink (mailMessages, selectedContact.getEmail());
-        app.registration().finish(confirmationLink,selectedContact.getUsername(), selectedContact.getPassword());
-        assertTrue(app.newSession().login(selectedContact.getUsername(), selectedContact.getPassword()));
+        String confirmationLink = app.user().findConfirmationLink(mailMessages, selectedUser.getEmail());
+        app.registration().finish(confirmationLink, selectedUser.getUsername(), selectedUser.getPassword());
+        assertTrue(app.newSession().login(selectedUser.getUsername(), selectedUser.getPassword()));
 
     }
+
     @AfterMethod(alwaysRun = true)
-    public void stopMailServer(){
+    public void stopMailServer() {
         app.mail().stop();
     }
 }
